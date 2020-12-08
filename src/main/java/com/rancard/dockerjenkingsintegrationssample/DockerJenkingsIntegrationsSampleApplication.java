75cc1b8a7c5f06989132de25963063f1e46c3bd9
Larry Ketone <kofie15@live.com>
2020-12-08 07:27:12 +0000

    first commit

---
+++ src/main/java/com/rancard/dockerjenkingsintegrationssample/DockerJenkingsIntegrationsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.rancard.dockerjenkingsintegrationssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkingsIntegrationsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJenkingsIntegrationsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rancard/dockerjenkingsintegrationssample/DockerJenkingsIntegrationsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancard.dockerjenkingsintegrationssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkingsIntegrationsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
