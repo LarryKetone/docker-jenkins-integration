f0bb5028d0fe6d91489b9fd1aba7bbe2936f5338
Lawrence Mpetey <lawrence.mpetey@gmail.com>
2021-06-10 19:37:56 +0000

    renamed kube folder

--- src/main/java/com/rancard/dockerjenkingsintegrationssample/controllers/MainController.java
+++ src/main/java/com/rancard/dockerjenkingsintegrationssample/controllers/MainController.java
@@ -1,12 +1,12 @@
 package com.rancard.dockerjenkingsintegrationssample.controllers;
 
+import com.rancard.dockerjenkingsintegrationssample.models.Customer;
 import com.rancard.dockerjenkingsintegrationssample.models.Employee;
 import com.rancard.dockerjenkingsintegrationssample.models.Response;
+import com.rancard.dockerjenkingsintegrationssample.repositories.CustomerRepository;
 import com.rancard.dockerjenkingsintegrationssample.repositories.EmployeeRepository;
 import com.rancard.dockerjenkingsintegrationssample.services.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,17 +18,19 @@ public class MainController {
     private final
     MainService mainService;
     private EmployeeRepository employeeRepository;
+    private CustomerRepository customerRepository;
 
     @Autowired
-    public MainController(MainService mainService, EmployeeRepository employeeRepository) {
+    public MainController(MainService mainService, EmployeeRepository employeeRepository, CustomerRepository customerRepository) {
         this.mainService = mainService;
         this.employeeRepository = employeeRepository;
+        this.customerRepository = customerRepository;
     }
 
     @GetMapping
     public Response getMain (HttpServletRequest request){
 
-        List<Employee> employees =  employeeRepository.findAll();
+        List<Customer> employees =  customerRepository.findAll();
         return new Response ("records size : " + employees.size(),Boolean.TRUE);
     }
 
